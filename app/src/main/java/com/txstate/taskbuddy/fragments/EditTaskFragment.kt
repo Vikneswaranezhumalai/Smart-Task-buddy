650fc7b90b01991984dfd1f2b389aaba7f971223
vikneswaranezhumalai <vigneshezhumalai@gmail.com>
2025-04-08 18:10:23 -0500

    Add EditTaskScreen using Jetpack Compose

--- app/src/main/java/com/txstate/taskbuddy/fragments/EditTaskFragment.kt
+++ app/src/main/java/com/txstate/taskbuddy/fragments/EditTaskFragment.kt
@@ -1,7 +1,41 @@
 package com.txstate.taskbuddy.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import com.txstate.taskbuddy.ui.theme.components.CommonToolbar
 
 
-class EditTaskFragment : Fragment(){
+class EditTaskFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(context = requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    CommonToolbar(
+                        title = "Edit Task Fragment ",
+                        navigationIcon = Icons.Default.ArrowBack,
+                        onBackButtonClick = {
+                            requireActivity().supportFragmentManager.popBackStack()
+                        },
+                        onCompletedTasksClick = null
+                    )
+
+                   // EditTaskContent()
+                }
+
+            }
+        }
+    }
 }
